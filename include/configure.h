f0ea16bf554559d38e175d7c1c4d78d12127eba6
hedgehogZDH <hedgehogzdh@gmail.com>
2014-03-31 00:40:58 +0800

    modify configure.h

--- include/configure.h
+++ include/configure.h
@@ -11,26 +11,39 @@
 #define __CONFIGURE_H__
 
 #include "common.h"
+
+#include <iostream>
+#include <fstream>
 #include <string>
+#include <map>
+
 using namespace std;
 
-class Conf
-{
+class Configure {
+
 public:
-    string getPath();
-    unsigned short getPort(){return _port;}
+	Configure();
+	Configure(string filename, string delimeter="=", string comment="#");
 
-    bool setPath(string path);
-    bool setPort(unsigned port);
-    bool loadFile();
 
-    Conf(string path="./www", unsigned short port=80)
-		: _path(path), _port(port)
-		{}
+	void parseFile(string filename="/etc/httpd.conf", string delimeter="=", string comment="#"); //解析Conf文件
+	bool fileExists(string filename);
+	string trim(const string str);	//去掉空格
+
+	string getValue(string key); 	//根据key返回value
+	bool keyExists(string key);
+
+	void render(); 	//输出结果
+
+	string getDelimeter() { return _delimeter; };
+	string getComment(){ return _comment; };
+	void setDelimeter(string delimeter) { _delimeter = delimeter; };
+	void setComment(string comment) {_comment = comment; };
 
 private:
-    string _path;
-    unsigned short _port;
+	map<string, string> conf_content;	//保存配置文件的key-value键值对
+	string _delimeter;	//key-value之间的分隔符
+	string _comment; 	//注释
 };
 
 #endif
